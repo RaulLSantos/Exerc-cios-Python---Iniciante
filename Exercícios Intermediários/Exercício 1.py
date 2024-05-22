126219708f93d81e1d2b181106954daf48c15564
RaulLSantos <raul.morais13@gmail.com>
2024-05-22 18:40:25 -0300

    Exercício1

--- Exercícios Intermediários/Exercício 1.py
+++ Exercícios Intermediários/Exercício 1.py
@@ -1,13 +1,46 @@
 '''Jogo da Forca:
-    Implemente o jogo da forca, onde o jogador tem que adivinhar uma palavra oculta.4'''
+    Implemente o jogo da forca, onde o jogador tem que adivinhar uma palavra oculta.'''
+import os
+os.system('cls')
 
 class JogodaForca():
     def __init__(self):
+        self.palavra = "Python"
+        self.tamanho_palavra = len(self.palavra)
+        
+        
 
-        self.palavra = "PyTHon"
-        self.palavra = self.palavra.lower()
+        self.LIMITE = 3
+        self.tentativas = 1
+        self.letra_correta = []
+        
+        while True:
+            
+            
+            self.palavra_mostrada = ""
+            if self.tentativas >= 1 and self.tentativas <= self.LIMITE:
+                self.letra = input(str("\nDigite uma palavra: "))
+                if self.letra.lower() in self.palavra.lower():
+                    self.letra_correta += self.letra.lower()
+                    self.tamanho_palavra -= 1
+                    print(f"\n Acertou! Letra: {self.letra} \n")
+                    if self.tamanho_palavra == 0:
+                        print(f"\n Parabéns, a palavra era {self.palavra.upper()}\n\n")
+                        break
+                    
+                else:
+                    self.tentativas += 1
+                    print(f"\n Infelizmente você errou \n")
 
-        self.letras_corretas = tuple()
+            else: 
+                print("\nVocê chegou ao limite de suas tentativas!\n")
+                break
 
-        for letra in self.palavra:
-            if letra in self.palavra:
+            for self.letra in self.palavra.lower():
+                if self.letra in self.letra_correta:
+                    self.palavra_mostrada += self.letra
+                else:
+                    self.palavra_mostrada += "_"
+            print("\n" + self.palavra_mostrada)
+              
+jogo_da_forca = JogodaForca()
